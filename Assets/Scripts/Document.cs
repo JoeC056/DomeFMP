04851bde827a214aec6209b446d392788a88eeef
Joe Clarkson <joe.clarkson056@gmail.com>
2026-03-08 19:23:59 +0000

    Bug Fix
    -Fixed issue that caused multiple instances of the document viewing application to be able to be opened

--- Assets/Scripts/Document.cs
+++ Assets/Scripts/Document.cs
@@ -6,8 +6,19 @@ public class Document : MonoBehaviour
     //////////////////////////////////////////////////////////////////////////////
     public void OnClick()
     {
-        ComputerManager.instance.OpenApplication(DocumentViewingApplication.instance.gameObject.GetComponent<Application>().application);
-        DocumentViewingApplication.instance.UpdateViewedDocument(this.gameObject);
+        ApplicationSO docViewingApplication = DocumentViewingApplication.instance.gameObject.GetComponent<Application>().application;
+
+        //Focuses app if already open, otherwise opens it
+        if (!ComputerManager.instance.openApplications.Contains(docViewingApplication))
+        {
+            ComputerManager.instance.OpenApplication(docViewingApplication);
+            DocumentViewingApplication.instance.UpdateViewedDocument(this.gameObject);
+        }
+        else
+        {
+            ComputerManager.instance.FocusApplication(docViewingApplication);
+            DocumentViewingApplication.instance.UpdateViewedDocument(this.gameObject);
+        }
     }
 
     //////////////////////////////////////////////////////////////////////////////
